baecd836a22b810c78c9fa71b68c938d7f2fef8b
DONDAJIN <hkazu.2@icloud.com>
2022-08-02 15:46:13 +0900

    add file defaultdic

---
+++ defaultdic.py
@@ -0,0 +1,25 @@
+#defaultdic
+n,m=map(int,input().split())
+S=list(map(int,input().split()))
+T=list(map(int,input().split()))
+from collections import defaultdict
+flag=defaultdict(int)
+for i in range(n):
+    flag[T[i]]=1
+for j in range(m):
+    if flag[S[j]]==1:
+        print('Yes')
+    else:
+        print('No')
+#While を使っても以下のように出来る。
+currentpos=0
+for st in T:
+    if S[currentpos]==st:
+        print('Yes')
+        currentpos=+1
+        break
+    else:
+        print('No')
+        currentpos=+1
+
+
